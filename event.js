3761c552d2bb551858e3c03764d35f4c46a49282
Satajyoti <dhawanil@yahoo.com>
2020-05-09 16:52:56 +0530

    Evntemiiter demo

--- event.js
+++ event.js
@@ -1,7 +1,30 @@
 var event=require('events')
+var util=require('util')
 var myeventemitter=new event.EventEmitter();
 myeventemitter.on('someevent',(msg)=>{
     console.log(msg);
 })
 
 myeventemitter.emit('someevent','my test event');
+
+var person=function(name){
+    this.name=name
+}
+util.inherits(person,event.EventEmitter)
+
+var james=new person('james')
+var sachin=new person('sachin')
+var virat=new person('virat')
+var persons=[james,sachin,virat]
+persons.forEach((p)=>{
+        p.on('speak',(msg)=>{
+                console.log(`${msg} said by ${p.name}`)
+        })
+})
+
+james.emit('speak','I want to ball');
+sachin.emit('speak','I want to bat');
+virat.emit('speak','I want to be captain');
+
+
+
